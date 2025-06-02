98f9ccac2750e75961660ea5941fab7a51cf96e5
Parithikrishnan <parithikrishnan001@gmail.com>
2025-06-02 08:03:31 +0530

    Every Scraped data are stored in a single file called Storing_file

--- instagram.py
+++ instagram.py
@@ -17,25 +17,29 @@ driver = webdriver.Firefox()
 driver.get('https://www.instagram.com/')
 t.sleep(3)
 username = driver.find_element(By.CSS_SELECTOR, "input[aria-label='Phone number, username, or email']")
-username.send_keys("username")   # Username needs to be added
+username.send_keys("user081759")   # Username needs to be added
 username.send_keys(Keys.TAB)
 t.sleep(2)
 password = driver.find_element(By.CSS_SELECTOR, "input[aria-label='Password']")
 t.sleep(1)
-password.send_keys("password")    # Password needs to be added 
+password.send_keys("parithi4122W")    # Password needs to be added 
 password.send_keys(Keys.RETURN)
 t.sleep(5)
 name_s = input("Enter the name you want to scrape that is automatically prefixed with #: ")
 searching_url = 'https://www.instagram.com/explore/search/keyword/?q=%23'+name_s
 driver.get(searching_url)
 response = driver.page_source
-t.sleep(5)
+t.sleep(7)
 search_results = driver.execute_script("return Array.from(document.querySelectorAll('a')).map(a => a.href);")
 opening_links = []
 for link in search_results:
     if link and "https://www.instagram.com/p/" in link:
         opening_links.append(link)
 count = 0
+
+with open ('Storing_file.txt','a') as f:
+    f.write(\n\nInstagram Data\n)
+
 for post_url in opening_links:
     driver.get(post_url)
     t.sleep(2)
@@ -45,7 +49,7 @@ for post_url in opening_links:
     current_time = datetime.now()
     formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
     data = 'Date:time : '+formatted_time+"\nContent : "+str(meta_tag)+"\nDetails : "+str(meta_tag['content'])+"\nLink : "+str(post_url)
-    with open('instagram_data.txt', 'a') as f:
+    with open('Storing_file.txt', 'a') as f:
         f.write(data+'\n\n')
         print(data)
         t.sleep(1)
